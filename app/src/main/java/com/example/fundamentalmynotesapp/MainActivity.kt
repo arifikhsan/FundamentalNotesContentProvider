ebc2529f826f8be8e2f766b69e05feb416cd60d0
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-11-21 17:44:29 +0700

    activities

--- app/src/main/java/com/example/fundamentalmynotesapp/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalmynotesapp/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.example.fundamentalmynotesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.NoteAddUpdateActivity
+import com.example.NoteAddUpdateActivity.Companion.EXTRA_NOTE
+import com.example.NoteAddUpdateActivity.Companion.EXTRA_POSITION
+import com.example.fundamentalmynotesapp.db.NoteHelper
+import com.example.fundamentalmynotesapp.entity.Note
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: NoteAdapter
+    private lateinit var noteHelper: NoteHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Notes"
+
+        rv_notes.layoutManager = LinearLayoutManager(this)
+        rv_notes.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        rv_notes.adapter = adapter
+
+        fab_add.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD)
+        }
+
+        noteHelper = NoteHelper.getInstance(applicationContext)
+        noteHelper.open()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) {
+            when (requestCode) {
+                NoteAddUpdateActivity.REQUEST_ADD -> if (resultCode == NoteAddUpdateActivity.RESULT_ADD) {
+                    val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE)
+
+                    adapter.addItem(note)
+                    rv_notes.smoothScrollToPosition(adapter.itemCount - 1)
+
+                    showSnackbarMessage("Satu item berhasil ditambahkan")
+                }
+
+                NoteAddUpdateActivity.REQUEST_UPDATE ->
+                    when (resultCode) {
+                        NoteAddUpdateActivity.RESULT_UPDATE -> {
+                            val note = data.getParcelableExtra<Note>(EXTRA_NOTE)
+                            val position = data.getIntExtra(EXTRA_POSITION, 0)
+
+                            adapter.updateItem(position, note)
+                            rv_notes.smoothScrollToPosition(position)
+
+                            showSnackbarMessage("Satu item berhasil diubah")
+                        }
+                        NoteAddUpdateActivity.RESULT_DELETE -> {
+                            val position = data.getIntExtra(EXTRA_POSITION, 0)
+                            adapter.removeItem(position)
+
+                            showSnackbarMessage("Satu item berhasil dihapus")
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(rv_notes, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        noteHelper.close()
     }
 }
---
+++ app/src/main/java/com/example/fundamentalmynotesapp/MappingHelper.kt
@@ -0,0 +1,5 @@
+package com.example.fundamentalmynotesapp
+
+object MappingHelper {
+    fun mapCursorToArrayList() {}
+}
